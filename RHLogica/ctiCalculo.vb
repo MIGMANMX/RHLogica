dafa4f366f65302f0f78c65b41a2a1e4b5bb44d6
Milton Ortega <MIGMANMX@users.noreply.github.com>
2017-09-28 19:01:30 -0500

    28/09/2017

--- RHLogica/ctiCalculo.vb
+++ RHLogica/ctiCalculo.vb
@@ -24,26 +24,88 @@ Public Class ctiCalculo
         rdr.Close() : rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
         Return dt
     End Function
-    Public Function datosCalculo(ByVal idempleado As Integer, ByVal chec As Date) As String()
+    Public Function datosHora(ByVal idchequeo As Integer) As String()
         Dim dbC As New SqlConnection(StarTconnStrRH)
         dbC.Open()
-        Dim cmd As New SqlCommand("SELECT idchequeo, chec, tipo FROM Chequeo Where chec ='" & chec & "'  AND idempleado=@idempleado   ORDER BY chec", dbC)
-        cmd.Parameters.AddWithValue("idempleado", idempleado)
-        cmd.Parameters.AddWithValue("chec", chec)
+        Dim cmd As New SqlCommand("Select Convert(varchar(10),chec, 8)as chec  from Chequeo  where idchequeo=@idchequeo ", dbC)
+        cmd.Parameters.AddWithValue("idchequeo", idchequeo)
+
         Dim rdr As SqlDataReader = cmd.ExecuteReader
         Dim dsP As String()
         If rdr.Read Then
-
-            ReDim dsP(3)
-            dsP(0) = rdr("idchequeo").ToString
-            dsP(1) = rdr("chec").ToString
-            dsP(2) = rdr("tipo").ToString
-
+            ReDim dsP(1)
+            dsP(0) = rdr("chec").ToString
         Else
             ReDim dsP(0) : dsP(0) = "Error: no se encuentra."
         End If
         rdr.Close() : rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
         Return dsP
     End Function
+    Public Function agregarHora(ByVal fech As Date, ByVal idempleado As Integer, ByVal horain As Date, ByVal horafn As Date, ByVal horas As TimeSpan) As String()
+        Dim ans As String()
 
+        Dim dbC As New SqlConnection(StarTconnStr)
+        dbC.Open()
+        Dim cmd As New SqlCommand("INSERT INTO Horas SELECT @idempleado,@horain,@horafn,@horas,@fecha", dbC)
+        cmd.Parameters.AddWithValue("fecha", fech)
+        cmd.Parameters.AddWithValue("idempleado", idempleado)
+        cmd.Parameters.AddWithValue("horain", horain)
+        cmd.Parameters.AddWithValue("horafn", horafn)
+        cmd.Parameters.AddWithValue("horas", horas)
+        cmd.ExecuteNonQuery()
+        Dim rdr As SqlDataReader = cmd.ExecuteReader
+        cmd.CommandText = "SELECT idHoras FROM Horas WHERE fecha = @fecha"
+        rdr = cmd.ExecuteReader
+        rdr.Read()
+        ReDim ans(1)
+        ans(0) = "Agregado."
+        ans(1) = rdr("idpuesto").ToString
+        rdr.Close()
+
+        'If rdr.HasRows Then
+        '    ReDim ans(0)
+        '    ans(0) = "Error: no se puede agregar, ya existe."
+        '    rdr.Close()
+        'Else
+        '    rdr.Close()
+        '    cmd.CommandText = "INSERT INTO Horas SELECT @idempleado,@horain,@horafn,@horas,@fecha"
+        '    cmd.Parameters.AddWithValue("idempleado", idempleado)
+        '    cmd.Parameters.AddWithValue("horain", horain)
+        '    cmd.Parameters.AddWithValue("horafn", horafn)
+        '    cmd.Parameters.AddWithValue("horas", horas)
+
+        '    cmd.ExecuteNonQuery()
+        '    cmd.CommandText = "SELECT idHoras FROM Horas WHERE fecha = @fecha"
+        '    rdr = cmd.ExecuteReader
+        '    rdr.Read()
+        '    ReDim ans(1)
+        '    ans(0) = "Agregado."
+        '    ans(1) = rdr("idpuesto").ToString
+        '    rdr.Close()
+        'End If
+        rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
+
+        Return ans
+    End Function
+    'Public Function datosHTotales(ByVal idempleado As Integer, ByVal chec As Date) As String()
+    '    Dim dbC As New SqlConnection(StarTconnStrRH)
+    '    dbC.Open()
+    '    Dim cmd As New SqlCommand("Select * From Chequeo where chec>=@chec AND chec <= '" & DateAdd(DateInterval.Day, 1, chec).ToString("yyyy-dd-MM") & "' AND idempleado=@idempleado Order BY chec ", dbC)
+    '    cmd.Parameters.AddWithValue("idempleado", idempleado)
+    '    cmd.Parameters.AddWithValue("chec", chec)
+    '    Dim rdr As SqlDataReader = cmd.ExecuteReader
+    '    Dim dsP As String()
+    '    If rdr.Read Then
+
+    '        ReDim dsP(3)
+    '        dsP(0) = rdr("idchequeo").ToString
+    '        dsP(1) = rdr("chec").ToString
+    '        dsP(2) = rdr("tipo").ToString
+
+    '    Else
+    '        ReDim dsP(0) : dsP(0) = "Error: no se encuentra."
+    '    End If
+    '    rdr.Close() : rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
+    '    Return dsP
+    'End Function
 End Class
